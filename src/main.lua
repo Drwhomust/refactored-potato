f2cca2b7bd32d01a1cff1868ea57400ef7ed454a
Drwhomust <rabbids8008@gmail.com>
2025-03-11 22:39:05 -0700

    added game map and make sure the cam does not go to the void

--- src/main.lua
+++ src/main.lua
@@ -3,6 +3,8 @@ function love.load()
     camera = require 'libraries/camera'
     love.graphics.setDefaultFilter("nearest", "nearest")
 
+    gameMap = sti('')
+
     time = 1
 
     player = {}
@@ -31,6 +33,29 @@ function love.update()
     end
 
     cam:lookAt(player.x, player.y)
+
+    local w = love.graphics.getWidth()
+    local h = love.graphics.getHeight()
+
+    if cam.x < w/2 then
+        cam.x = w/2
+    end
+
+    if cam.y < h/2 then
+        cam.y = h/2
+    end
+
+    local mapW = gameMap.width * gameMap.tilewidth
+    local mapH = gameMap.height * gameMap.tileheight
+
+    if cam.x > (mapW - w/2) then
+        cam.x = (mapW - w/2)
+    end
+
+    if cam.y > (mapH - h/2) then
+        cam.y = (mapH - h/2)
+    end
+end
 end
 
 function love.draw()
